ab26394d750683658c1110e2a897a9a5616afa44
TonyJem <a.jemanov@gmail.com>
2022-05-14 14:50:46 +0300

    Remove storyboard

--- PerevalApp/PerevalApp/WelcomeVC.swift
+++ PerevalApp/PerevalApp/WelcomeVC.swift
@@ -1,10 +1,12 @@
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .green
     }
 
 
