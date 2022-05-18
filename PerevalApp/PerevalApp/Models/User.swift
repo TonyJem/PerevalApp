a997ae46403429d1ceee1fa5ef20a39e90091575
TonyJem <a.jemanov@gmail.com>
2022-05-18 18:18:32 +0300

    Create User struct

---
+++ PerevalApp/PerevalApp/Models/User.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct User: Codable {
+    
+    var surname: String
+    var name: String
+    var patronymic: String
+    var email: String
+    var phone: String
+    var mediaLink: String
+    
+    init(surname: String,
+         name: String,
+         patronymic: String,
+         email: String,
+         phone: String,
+         mediaLink: String) {
+        self.surname = surname
+        self.name = name
+        self.patronymic = patronymic
+        self.email = email
+        self.phone = phone
+        self.mediaLink = mediaLink
+    }
+}
