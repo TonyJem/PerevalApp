acc4a1fd4c7fc4d7ef54dd5b822dc3802eef9193
TonyJem <a.jemanov@gmail.com>
2022-05-20 19:19:13 +0300

    Create Models file

---
+++ PerevalApp/PerevalApp/Models/Models.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+struct MountainPassModel {
+    var title: String
+    var addTime: String
+}
+
+// MARK: - Coords
+struct MountainPassCoordinates {
+    var latitude: String
+    var longitude: String
+    var height: String
+}
+
+// MARK: - Image
+struct MountainPassImage {
+    var url: String
+    var title: String
+}
+
+// MARK: - Level
+struct MountainPassCategory {
+    var summer: String
+    var autumn: String
+}
+
+// MARK: - User
+struct MountainPassUser {
+    var email: String
+    var phone: String
+    var fam: String
+    var name: String
+}
